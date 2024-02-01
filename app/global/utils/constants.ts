40ad82406584914aa42e5d94da8abcef8e2c8425
Marvin Kiseka <kisekamarvin1@gmail.com>
2024-02-01 13:14:53 +0300

    api domain

--- app/global/utils/constants.ts
+++ app/global/utils/constants.ts
@@ -1,15 +1,15 @@
 
 export enum APP_STATUS {
-    IDLE= "idle",
-    PENDING= "pending",
-    ERROR= "error",
-    SUCCESS= "success",
+    IDLE = "idle",
+    PENDING = "pending",
+    ERROR = "error",
+    SUCCESS = "success",
 }
-export const BASE_URL="http://localhost:8000"
+// export const BASE_URL="http://localhost:8000"
+export const BASE_URL = "http://23.88.114.42:8081"
+
 export const STORAGE_URL = "https://storage.googleapis.com";
-
-
 export enum USER_TYPES {
-    VISITOR= "visitor",
-    FOTIS_STAFF= "fotis_staff",
+    VISITOR = "visitor",
+    FOTIS_STAFF = "fotis_staff",
 }
